b363cb2d61a84548e5bd596794fa19646c250505
Tim Nelson <tnelly27@gmail.com>
2010-03-05 20:10:21 -0600

    updated copyright year to 2010

--- src/main/scala/com/eltimn/scamongo/Mongo.scala
+++ src/main/scala/com/eltimn/scamongo/Mongo.scala
@@ -1,7 +1,7 @@
 package com.eltimn.scamongo
 
 /*
- * Copyright 2009 Tim Nelson
+ * Copyright 2010 Tim Nelson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
